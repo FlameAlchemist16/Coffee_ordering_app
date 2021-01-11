1c11733c8c78df25f4b3fcbe9a201a4acec59413
Mridul Khare <54219082+FlameAlchemist16@users.noreply.github.com>
2021-01-11 16:08:33 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.android.justjava;
+
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.NumberFormat;
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+    int quantity = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(quantity);
+        displayprice(quantity*10);
+    }
+    public void increment(View view){
+        quantity = quantity +1;
+        display(quantity);
+    }
+    public void decrement(View view){
+        if(quantity>0) quantity = quantity -1;
+        display(quantity);
+    }
+    public void reset(View view){
+        quantity =0;
+        display(quantity);
+        displayprice(quantity);
+    }
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+    private void displayprice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+}
